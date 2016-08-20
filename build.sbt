076bfdfd172716cef1ed065987e54b4d45482797
Махетов Сергей <Profitfx@mail.ru>
2016-08-21 00:41:28 +0500

    add steps

--- build.sbt
+++ build.sbt
@@ -3,7 +3,6 @@ name := "skb-test"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-
 // https://mvnrepository.com/artifact/org.scalatest/scalatest_2.11
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "3.0.0"
 // https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml-schemas
@@ -12,5 +11,5 @@ libraryDependencies += "org.apache.poi" % "poi-ooxml-schemas" % "3.14"
 libraryDependencies += "org.apache.poi" % "poi-ooxml" % "3.14"
 // https://mvnrepository.com/artifact/org.apache.poi/poi
 libraryDependencies += "org.apache.poi" % "poi" % "3.14"
-
-    
+// https://mvnrepository.com/artifact/org.scalaj/scalaj-http_2.11
+libraryDependencies += "org.scalaj" % "scalaj-http_2.11" % "2.3.0"
--- src/test/scala/GetTest.scala
+++ src/test/scala/GetTest.scala
@@ -1,4 +1,6 @@
-import org.scalatest.FreeSpec
+import org.scalatest.{FreeSpec, Matchers}
+
+import scalaj.http.{Http, HttpResponse}
 
 /**
   * Created by Enot on 20.08.2016.
@@ -12,5 +14,44 @@ class GetTest extends FreeSpec{
   }
   "Bad test" in {
     assert(false)
+  }
+}
+
+
+class MainTest extends FreeSpec with Matchers {
+  /** Что надо протестировать:
+    * # Отправку сообщения ORDERS через АПИ.
+    * # Наличие ошибки при повторной отправке сообщения без изменений.
+    * # В интерфейсе поставщика проверить наличие отправленного сообщения.
+    * # В карточке сообщения на вкладке "Заявка" проверить:
+    * ## Корректность названия товара №2 и №4 (названия можно найти и менять из файла ORDERS).
+    * ## Скачать xlsx и проверить корректность названия товара №2 и №4 в скачанном файле.
+    */
+
+  "Отправка сообщения ORDERS через АПИ" in {
+    val response: HttpResponse[String] = Http("https://test-edi-api.kontur.ru/V1/Boxes/GetBoxesInfo").header("Authorization","profitfx-e1d4b253-79d2-41c2-9304-be97d6bb48cd").asString
+    println(response.body)
+    println(response.code)
+    println(response.headers)
+    println(response.cookies)
+  }
+
+  "Наличие ошибки при повторной отправке сообщения без изменений" in {
+
+  }
+
+  "В интерфейсе поставщика проверить наличие отправленного сообщения" in {
+
+  }
+
+  "В карточке сообщения на вкладке \"Заявка\" проверить" - {
+    "Корректность названия товара №2 и №4 (названия можно найти и менять из файла ORDERS)" in {
+
+    }
+
+    "Скачать xlsx и проверить корректность названия товара №2 и №4 в скачанном файле" in {
+
+    }
+
   }
 }
