f5adae557ef32f0240ed17d1cbbf7153d9d6f694
Махетов Сергей <Profitfx@mail.ru>
2016-08-20 10:38:19 +0500

    First commit

---
+++ src/test/scala/GetTest.scala
@@ -0,0 +1,13 @@
+import org.scalatest.FreeSpec
+
+/**
+  * Created by Enot on 20.08.2016.
+  */
+class GetTest extends FreeSpec{
+  "Good test" in {
+    assert(true)
+  }
+  "Bad test" in {
+    assert(false)
+  }
+}
