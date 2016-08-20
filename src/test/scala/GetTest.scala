8a1eaef39afb12d5ec57556423a3fb305ba19963
Махетов Сергей <Profitfx@mail.ru>
2016-08-20 12:25:24 +0500

    Add comment

--- src/test/scala/GetTest.scala
+++ src/test/scala/GetTest.scala
@@ -2,6 +2,9 @@ import org.scalatest.FreeSpec
 
 /**
   * Created by Enot on 20.08.2016.
+  *
+  * работа с экселем
+  * https://tproger.ru/translations/how-to-read-write-excel-file-java-poi-example/
   */
 class GetTest extends FreeSpec{
   "Good test" in {
