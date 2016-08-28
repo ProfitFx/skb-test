6e1bef03cc891d353b49f7e14d40f9ffd283d6fb
Махетов Сергей <Profitfx@mail.ru>
2016-08-28 18:33:24 +0500

    add json processing

--- build.sbt
+++ build.sbt
@@ -3,23 +3,24 @@ name := "skb-test"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-// https://mvnrepository.com/artifact/org.scalatest/scalatest_2.11
+
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "3.0.0" % "test->*"
-// https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml-schemas
+
 libraryDependencies += "org.apache.poi" % "poi-ooxml-schemas" % "3.14"
-// https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
+
 libraryDependencies += "org.apache.poi" % "poi-ooxml" % "3.14"
-// https://mvnrepository.com/artifact/org.apache.poi/poi
+
 libraryDependencies += "org.apache.poi" % "poi" % "3.14"
-// https://mvnrepository.com/artifact/org.scalaj/scalaj-http_2.11
+
 libraryDependencies += "org.scalaj" % "scalaj-http_2.11" % "2.3.0"
-// https://mvnrepository.com/artifact/org.json4s/json4s-native_2.11
+
 libraryDependencies += "org.json4s" % "json4s-native_2.11" % "3.4.0"
 
 libraryDependencies += "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.5"
 
-
 libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.53.1"
+
+libraryDependencies += "com.typesafe" % "config" % "1.3.0"
 
 //libraryDependencies += "org.pegdown" % "pegdown" % "1.6.0"
 
--- src/test/scala/GetTest.scala
+++ src/test/scala/GetTest.scala
@@ -1,8 +1,13 @@
 import java.io.FileInputStream
 
+import com.typesafe.config.ConfigFactory
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import org.scalatest.{FreeSpec, Matchers}
+
 import scalaj.http.Http
+
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 /**
   * Created by Enot on 20.08.2016.
@@ -22,26 +27,40 @@ class GetTest extends FreeSpec{
 
 class ApiTest extends FreeSpec with Matchers {
 
+  val conf = ConfigFactory.load
+
 
   "Отправка сообщения ORDERS через АПИ"  - {
-    var token = ""
 
-    "Получение токена авторизации "in {
-      val authHeader = "KonturEdiAuth konturediauth_api_client_id=profitfx-e1d4b253-79d2-41c2-9304-be97d6bb48cd , konturediauth_login=profitfx@mail.ru, konturediauth_password=Ko624205!"
-      val response = Http("https://test-edi-api.kontur.ru/V1/Authenticate").postForm.header("Authorization", authHeader).asString
+    var token = ""
+    var boxId = ""
+    var authHeader = ""
+
+    val url = conf.getString("api.url")
+    val login = conf.getString("client.login")
+    val password = conf.getString("client.password")
+    var id = conf.getString("client.id")
+
+    "Получение токена авторизации и формирование заголовка для последкющих вызовов API" in {
+      val firstAuthHeader = s"KonturEdiAuth konturediauth_api_client_id=$id, konturediauth_login=$login, konturediauth_password=$password"
+      val response = Http(s"$url/Authenticate").postForm.header("Authorization", firstAuthHeader).asString
       token = response.body
+      authHeader = s"KonturEdiAuth konturediauth_api_client_id=$id, konturediauth_token=$token"
       println(token)
     }
 
-    "Получение BoxesInfo" in {
-      val authHeader = s"KonturEdiAuth konturediauth_api_client_id=profitfx-e1d4b253-79d2-41c2-9304-be97d6bb48cd , konturediauth_token=$token"
-      val response = Http("https://test-edi-api.kontur.ru/V1/Boxes/GetBoxesInfo").header("Authorization", authHeader).asString
+    "Получение BoxesInfo и извлечение из него идентификатора ящика" in {
+      val response = Http(s"$url/Boxes/GetBoxesInfo").header("Authorization", authHeader).asString
       println(response.body)
+      val boxJson = parse(response.body)
+      boxId = ((boxJson \ "Boxes").children(0) \"Id").values.toString
+      println(boxId)
     }
 
-    "Отправка сообщения" in {
-      val authHeader = s"KonturEdiAuth konturediauth_api_client_id=profitfx-e1d4b253-79d2-41c2-9304-be97d6bb48cd , konturediauth_token=$token"
-      val response = Http("https://test-edi-api.kontur.ru/V1/Messages/SendMessage?boxId=e4e8b56d-3390-4e29-b7f5-169a83efacab").postData("UNB+UNOE:3+1277777777773+1377777777770+20111204:1200+12345555'\nUNH+01+ORDERS:D:01B:UN:EAN010'\nBGM+220+NN01+9'\nDTM+137:201112041159:203'\nDTM+2:201112051200:203'\nRFF+CT:contractNumber'\nDTM+171:20131220:102'\nNAD+SU+1377777777770::9'\nRFF+YC1:21546'\nNAD+BY+1277777777773::9'\nNAD+DP+1277777777773::9'\nCUX+2:RUB:9'\nLIN+1++4600375914498:SRV'\nPIA+1+68778578:SA'\nPIA+1+358748:IN'\nIMD+F++:::GoodItem1'\nQTY+21:30.555:KGM'\nMOA+203:1527.75'\nPRI+AAA:50.0000'\nLIN+2++4366687650157:SRV'\nPIA+1+456:SA'\nPIA+1+358746:IN'\nIMD+F++:::GoodItem2'\nQTY+21:40:PCE'\nMOA+203:1865.58'\nPRI+AAA:46.6395'\nLIN+3++4600375914474:SRV'\nPIA+1+615464:SA'\nQTY+21:0:PCE'\nPRI+AAA:56.7800'\nLIN+4++4600375001112:SRV'\nPIA+1+111:SA'\nPIA+1+333:IN'\nIMD+F++:::GoodItme3'\nQTY+21:100:PCE'\nMOA+203:99995.00'\nPRI+AAA:999.95'\nUNS+S'\nMOA+125:106568.2760'\nCNT+2:7'\nUNT+48+01'\nUNZ+1+1'").header("Authorization", authHeader).param("boxId","e4e8b56d-3390-4e29-b7f5-169a83efacab").asString
+    "Отправка сообщения" ignore {
+      val postBody = scala.io.Source.fromFile("taskFiles\orders.txt").mkString
+      val response = Http(s"$url/Messages/SendMessage?boxId=$boxId").postData(postBody).asString
+      // val response = Http(s"$url/Messages/SendMessage?boxId=$boxId").postData("UNB+UNOE:3+1277777777773+1377777777770+20111204:1200+12345555'\nUNH+01+ORDERS:D:01B:UN:EAN010'\nBGM+220+NN01+9'\nDTM+137:201112041159:203'\nDTM+2:201112051200:203'\nRFF+CT:contractNumber'\nDTM+171:20131220:102'\nNAD+SU+1377777777770::9'\nRFF+YC1:21546'\nNAD+BY+1277777777773::9'\nNAD+DP+1277777777773::9'\nCUX+2:RUB:9'\nLIN+1++4600375914498:SRV'\nPIA+1+68778578:SA'\nPIA+1+358748:IN'\nIMD+F++:::GoodItem1'\nQTY+21:30.555:KGM'\nMOA+203:1527.75'\nPRI+AAA:50.0000'\nLIN+2++4366687650157:SRV'\nPIA+1+456:SA'\nPIA+1+358746:IN'\nIMD+F++:::GoodItem2'\nQTY+21:40:PCE'\nMOA+203:1865.58'\nPRI+AAA:46.6395'\nLIN+3++4600375914474:SRV'\nPIA+1+615464:SA'\nQTY+21:0:PCE'\nPRI+AAA:56.7800'\nLIN+4++4600375001112:SRV'\nPIA+1+111:SA'\nPIA+1+333:IN'\nIMD+F++:::GoodItme3'\nQTY+21:100:PCE'\nMOA+203:99995.00'\nPRI+AAA:999.95'\nUNS+S'\nMOA+125:106568.2760'\nCNT+2:7'\nUNT+48+01'\nUNZ+1+1'").header("Authorization", authHeader).param("boxId","e4e8b56d-3390-4e29-b7f5-169a83efacab").asString
       println(response.body)
     }
   }
@@ -57,6 +76,7 @@ class ApiTest extends FreeSpec with Matchers {
     }
 
     "Скачать xlsx" ignore {
+
     }
 
   }
@@ -80,4 +100,11 @@ class ExcelTest extends FreeSpec with Matchers{
   }
 }
 
-class WebTest extends FreeSpecWithBrowserScaledScreen
+class WebTest extends FreeSpecWithBrowserScaledScreen {
+
+  val conf = ConfigFactory.load
+  val baseUrl = conf.getString("web.url")
+  go to baseUrl
+
+
+}
